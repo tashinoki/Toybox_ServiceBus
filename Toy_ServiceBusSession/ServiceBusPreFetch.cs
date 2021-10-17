29f9867d7b8be980488c3934efbb3451f2305cdb
tashinoki <kenya.112163@gmail.com>
2021-10-17 20:08:34 +0900

    add message receiver

--- Toy_ServiceBusSession/ServiceBusPreFetch.cs
+++ Toy_ServiceBusSession/ServiceBusPreFetch.cs
@@ -2,16 +2,21 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Threading.Tasks;
 
 namespace Toy_ServiceBusSession
 {
     public static class ServiceBusPreFetch
     {
         [FunctionName("ServiceBusPreFetch")]
-        public static void Run(
+        public async static Task Run(
             [ServiceBusTrigger("toy-prefetch", Connection = "ServiceBusConnection")]string myQueueItem,
+            MessageReceiver messageReceiver,
             ILogger log)
         {
+            Console.WriteLine(messageReceiver.PrefetchCount);
+            await Task.Delay(5000);
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
     }
